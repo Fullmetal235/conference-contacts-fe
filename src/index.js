d1aa4faf7dacb424a705273d64831de2622ab8ec
zachary-peasley <zpman95@hotmail.com>
2019-12-02 11:31:45 -0600

    Added Mixpanel Analytic

--- src/index.js
+++ src/index.js
@@ -7,10 +7,10 @@ import history from './utils/history';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
 import './index.css';
+import mixpanel from 'mixpanel-browser';
+import { MixpanelProvider, MixpanelConsumer } from 'react-mixpanel';
 
-
-
-
+mixpanel.init("b02a197733f5750bb0d7c6bb0bdc4418");
 
 
 
@@ -28,7 +28,6 @@ export const client = new ApolloClient({
 
 
 
-
 const onRedirectCallback = appState => {
   history.push(appState && appState.targetUrl
     ? appState.targetUrl
@@ -37,7 +36,15 @@ const onRedirectCallback = appState => {
 };
 
 
+class INeedMixpanel extends React.Component {
+  componentDidMount() {
+      this.props.mixpanel.track('Hello mixpanel!');
+  }
 
+  render() {
+    return <div></div>
+}
+}
 // Component that will be aware of Mixpanel existence - it will pass Mixpanel
 // instance down to it's child
 
@@ -45,7 +52,12 @@ const onRedirectCallback = appState => {
 // Proper Mixpanel consumer, it gets Mixpanel instance by props
 
 	
-
+const Mix = () => 
+    <div>
+        <MixpanelConsumer>
+            {mixpanel => <INeedMixpanel mixpanel={mixpanel}/>}
+        </MixpanelConsumer>
+    </div>;
 	
 
 
@@ -56,6 +68,7 @@ const render = () => {
   const App = require('./app/App').default;
 
   ReactDOM.render(
+    <MixpanelProvider mixpanel={mixpanel}>
     <Auth0Provider
       domain={config.domain}
       client_id={config.clientId}
@@ -66,9 +79,11 @@ const render = () => {
       <ApolloProvider client={client}>
         <Router history={history}>
           <App />
+          <Mix />
         </Router>
       </ApolloProvider>
-    </Auth0Provider>,
+    </Auth0Provider>
+    </MixpanelProvider>,
     document.getElementById('root')
   );
 };
